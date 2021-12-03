a4bea94ae9fbe78580f963a23ce3f6341b947e82
T.Michel <theo.michel2@outlook.fr>
2021-12-03 11:29:26 +0100

    window closed on background and arrow click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 let allH1 = document.querySelectorAll('h1');//On stock tout les h1 clickable dans un tableau
 let currentH1Window;
+let currentDiv;
 
 allH1.forEach(h1 => { //On ajoute une boucle foreach pour ajouter un eventlistener à tous le tableau de h1
     h1.addEventListener("click",function(e){
+        e.stopPropagation(); //pour que l'évenement de se propage pas à toute la page
+        //On commence par fermer toutes les fenêtres ouvertes
+        allH1.forEach(otherH1 => {
+            currentDiv = document.querySelector("#" +otherH1.parentElement.id+" div");
+            if(currentDiv.classList.value== "show"){
+                 currentDiv.classList.replace("show","hide");
+                }
+        });
+        //Puis on s'occupe d'ouvrir ou cacher le texte correspondant à l'élément cliquer.
         currentH1Window = document.querySelector("#" +h1.parentElement.id+" div");//On sélectionne le text à afficher du h1 actuel cliqué
         if(currentH1Window.classList.value == "hide"){ //On vérifie la class actuel du h1
-            document.querySelector("#" +h1.parentElement.id+" div").classList.replace("hide", "show");
+            currentH1Window.classList.replace("hide", "show");
         }
         else{
-            document.querySelector("#" +h1.parentElement.id+" div").classList.replace("show", "hide");
+            currentH1Window.classList.replace("show", "hide");
         }
     });
 });
+
+ window.addEventListener("click", function(){
+     allH1.forEach(h1 => {
+        if(document.querySelector("#" +h1.parentElement.id+" div").classList.value=="show"){
+            document.querySelector("#" +h1.parentElement.id+" div").classList.replace("show", "hide");
+        }
+     });
+ });
